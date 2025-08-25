1d7da9c38f2f7b5798a3cc9efa4c96c1ec085a47
PsGhosh <pgsarothi2005@gmail.com>
2025-08-25 22:57:44 +0600

    Add basic calculator functionality for addition, subtraction, multiplication, and division

---
+++ Calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum, minus, product, devide;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    minus = a - b;
+    product = a * b;
+    devide = a / b;
+    printf("Sum: %d\n", sum);
+    printf("Minus: %d\n", minus);
+    printf("Product: %d\n", product);
+    printf("Divide: %d\n", devide);
+    return 0;
+}
